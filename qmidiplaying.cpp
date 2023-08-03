bd9ca364975734c2e2422a71f3c7097a595845d8
brucederekhans <brucederekhans@gmail.com>
2023-08-03 10:00:23 +0800

    declare temporary variable t4Bytes

--- qmidiplaying.cpp
+++ qmidiplaying.cpp
@@ -36,6 +36,7 @@ void QMIDIPlaying::execute()
         {
             QMIDI midi;
             memset(&midi, 0, sizeof(QMIDI));
+            unsigned char t4Bytes[4];
 
             fclose(pMIDIFile);
         }
