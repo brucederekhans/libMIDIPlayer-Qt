3f311bfb5833f4c3c83f03af609ada15a17fdd68
brucederekhans <brucederekhans@gmail.com>
2023-08-06 15:03:53 +0800

    implement readUShortFromMIDIFile()

--- qmidi.cpp
+++ qmidi.cpp
@@ -1,3 +1,10 @@
 #include "qmidi.h"
 
 const char MThd[5] = "MThd";
+
+unsigned short readUShortFromMIDIFile(FILE * pMIDIFile)
+{
+    unsigned char hiByte = static_cast<unsigned char>(fgetc(pMIDIFile));
+    unsigned char loByte = static_cast<unsigned char>(fgetc(pMIDIFile));
+    return static_cast<unsigned short>(MAKEWORD(loByte, hiByte));
+}
--- qmidi.h
+++ qmidi.h
@@ -5,17 +5,19 @@
 #include <mmsystem.h>
 
 struct QMIDI{
-	unsigned char isPlaying;
-	unsigned char isPaused;
-	unsigned char isStopRequested;
-	unsigned short countTracks;
-	unsigned short countTicks;
-	unsigned int tempo;
-	double tickLength;
-	unsigned char channels[16][128];
-	double currentTime;
+    unsigned char isPlaying;
+    unsigned char isPaused;
+    unsigned char isStopRequested;
+    unsigned short countTracks;
+    unsigned short countTicks;
+    unsigned int tempo;
+    double tickLength;
+    unsigned char channels[16][128];
+    double currentTime;
 };
 
 extern const char MThd[5];
+
+unsigned short readUShortFromMIDIFile(FILE * pMIDIFile);
 
 #endif
