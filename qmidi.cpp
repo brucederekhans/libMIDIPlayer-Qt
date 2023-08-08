e74651520b1fd5629413d65e085976b3059e5512
brucederekhans <brucederekhans@gmail.com>
2023-08-08 08:20:26 +0800

    check whether the pointer of file is null in readUShortFromMIDIFile() or not

--- qmidi.cpp
+++ qmidi.cpp
@@ -4,10 +4,17 @@ const char MThd[5] = "MThd";
 
 unsigned long long readUShortFromMIDIFile(unsigned short * pDst, FILE * pMIDIFile)
 {
-    unsigned char hiByte = static_cast<unsigned char>(fgetc(pMIDIFile));
-    unsigned char loByte = static_cast<unsigned char>(fgetc(pMIDIFile));
-    (*pDst) = static_cast<unsigned short>(MAKEWORD(loByte, hiByte));
-    return 2;
+    if(pMIDIFile != nullptr)
+    {
+        unsigned char hiByte = static_cast<unsigned char>(fgetc(pMIDIFile));
+        unsigned char loByte = static_cast<unsigned char>(fgetc(pMIDIFile));
+        (*pDst) = static_cast<unsigned short>(MAKEWORD(loByte, hiByte));
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 unsigned long long readUIntFromMIDIFile(unsigned int * pDst, FILE * pMIDIFile)
