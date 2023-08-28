733e0d08a91cd36190dbbf6440aae06c05e0aaf1
brucederekhans <brucederekhans@gmail.com>
2023-08-28 08:41:51 +0800

    check whether it is time to trigger process or not

--- qmidiplaying.cpp
+++ qmidiplaying.cpp
@@ -141,6 +141,11 @@ void QMIDIPlaying::execute()
                                         midiTrackHeaders[iTrack].deltaTime = readVLQFromMIDITrackHeader(&midiTrackHeaders[iTrack]) * midi.tickLength;
                                         midiTrackHeaders[iTrack].triggerTime = tCurrentTime + midiTrackHeaders[iTrack].deltaTime;
                                     }
+
+                                    while(tCurrentTime >= midiTrackHeaders[iTrack].triggerTime)
+                                    {
+                                        //
+                                    }
                                 }
                                 else
                                 {
