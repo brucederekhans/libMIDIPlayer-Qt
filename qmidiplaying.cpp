ee8a17bcc37696a3fffc5b192ce01b7cf6af3984
brucederekhans <brucederekhans@gmail.com>
2023-09-11 08:47:55 +0800

    read key from the track when the high nybble equals 0x0A

--- qmidiplaying.cpp
+++ qmidiplaying.cpp
@@ -240,6 +240,10 @@ void QMIDIPlaying::execute()
                                                 unsigned char velocity = readByteFromMIDITrackHeader(midiTrackHeaders[iTrack]);
                                                 setNoteOnOff(1, key, velocity, this->volumePercentage, loNybble, &midi, &hMIDIOut);
                                             }
+                                            else if(hiNybble == 0x0A)
+                                            {
+                                                unsigned char key = readByteFromMIDITrackHeader(midiTrackHeaders[iTrack]);
+                                            }
                                         }
                                     }
                                 }
