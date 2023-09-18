2128c3d59a755230283756c7514ef430d7398b03
brucederekhans <brucederekhans@gmail.com>
2023-09-18 08:42:31 +0800

    update temporary variable tCurrentTime after reading tracks

--- qmidiplaying.cpp
+++ qmidiplaying.cpp
@@ -306,6 +306,7 @@ void QMIDIPlaying::execute()
                                 }
                             }
                         }
+                        tCurrentTime = getHighResolutionTime();
                     }
                 }
             }
