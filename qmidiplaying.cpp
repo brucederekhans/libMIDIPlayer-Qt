3068517968b6a11788cae3a212bf7bcd5b005789
brucederekhans <brucederekhans@gmail.com>
2023-09-25 05:27:37 +0800

    break while loop if try closing more than 100 times

--- qmidiplaying.cpp
+++ qmidiplaying.cpp
@@ -359,10 +359,16 @@ void QMIDIPlaying::execute()
                     Sleep(10);
                     midiOutReset(hMIDIOut);
                     MMRESULT resultMIDIOutClose;
+                    unsigned short iTryClosing = 0;
                     do
                     {
                         resultMIDIOutClose = midiOutClose(hMIDIOut);
                         Sleep(10);
+                        iTryClosing++;
+                        if(iTryClosing >= 100)
+                        {
+                            break;
+                        }
                     }while(resultMIDIOutClose != MMSYSERR_NOERROR);
                 }
             }
