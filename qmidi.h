2f00325cab9ba365e2200372ed54468c640267ba
brucederekhans <brucederekhans@gmail.com>
2023-10-28 23:41:09 +0800

    do not define channels as member of QMIDI

--- qmidi.h
+++ qmidi.h
@@ -11,7 +11,6 @@ namespace QMIDI{
         unsigned short countTicks;
         unsigned int tempo;
         double tickLength;
-        unsigned char channels[16][128];
         double currentTime;
     };
 
