11187412394eb79105fd8e0d94a34305c4d16ca1
brucederekhans <brucederekhans@gmail.com>
2023-10-29 10:49:50 +0800

    delete redundant parameter when call setNoteOnOff()

--- qmidiplaying.cpp
+++ qmidiplaying.cpp
@@ -266,13 +266,13 @@ void QMIDI::QMIDIPlaying::execute()
                                                 {
                                                     unsigned char key = readByteFromMIDITrackHeader(&midiTrackHeaders[jTrack]);
                                                     unsigned char velocity = readByteFromMIDITrackHeader(&midiTrackHeaders[jTrack]);
-                                                    setNoteOnOff(0, key, velocity, this->volumePercentage, loNybble, &midi, &hMIDIOut);
+                                                    setNoteOnOff(0, key, velocity, this->volumePercentage, loNybble, &hMIDIOut);
                                                 }
                                                 else if(hiNybble == 0x09)
                                                 {
                                                     unsigned char key = readByteFromMIDITrackHeader(&midiTrackHeaders[jTrack]);
                                                     unsigned char velocity = readByteFromMIDITrackHeader(&midiTrackHeaders[jTrack]);
-                                                    setNoteOnOff(1, key, velocity, this->volumePercentage, loNybble, &midi, &hMIDIOut);
+                                                    setNoteOnOff(1, key, velocity, this->volumePercentage, loNybble, &hMIDIOut);
                                                 }
                                                 else if(hiNybble == 0x0A)
                                                 {
